1f3a59f7d48fa74ae259b5286dd237d0e8d907f3
jaoangeloni <joaopedroangeloni@gmail.com>
2025-11-03 20:23:55 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3001);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
