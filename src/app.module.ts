ffc6ee2a9d66f0a4ce5d8308d443e3a72018aef8
jaoangeloni <joaopedroangeloni@gmail.com>
2025-11-05 21:37:33 -0300

    feat: document module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DocumentModule } from './modules/document/document.module';
 
 
 @Module({
@@ -7,6 +8,8 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }),],
+    }),
+    DocumentModule
+  ],
 })
 export class AppModule { }
